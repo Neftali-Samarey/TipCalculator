b7bf6c926dff7a25a9024bea7091260c0d8981ca
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2019-09-05 17:46:37 -0400

    Updated Display component:

--- app/components/display-controller.js
+++ app/components/display-controller.js
@@ -1,4 +1,10 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+   
+    model() { 
+        // Look up on routing values to the display component
+        // Also FYI: https://www.geos.ed.ac.uk/~smudd/NMDM_Course/html/more_advanced_github.html
+        return 100.00;
+    }
 });
