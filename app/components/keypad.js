988f1ffa74456e64ea290e29880cbd4b2acef08d
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2019-09-06 11:49:04 -0400

    Logic Implementation

--- app/components/keypad.js
+++ app/components/keypad.js
@@ -1,4 +1,25 @@
 import Component from '@ember/component';
 
+
+// Main Accumulator for the total values
+var accumulator = 0;
+
 export default Component.extend({
+    actions: {
+        one(post) {
+            post = 1;
+            alert("Clicked on button " + post);
+            callThisFunction();
+        },
+        two(post) {
+            post = 2;
+            alert("Clicked on button " + post)
+        }
+    }
 });
+
+
+
+function callThisFunction( ) {
+    alert("Fbnction was called succesfully");
+}
