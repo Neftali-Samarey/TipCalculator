60fcddeb26fb5321ad558a0b4b7008d7672f19fa
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2019-09-09 01:33:50 -0400

    UI enhancements

--- app/components/digit-display.js
+++ app/components/digit-display.js
@@ -1,6 +1,23 @@
 import Component from '@ember/component';
 
 // This will tie in and display the digits
+// This value willl stay local to the controllelr for the display
+ let defaultValue = 0;
 
 export default Component.extend({
+
+    model() {
+        let v = relayGivenValue(10);
+        return 0.0;
+    }
+    
 });
+
+
+
+function relayGivenValue(inputValues) {
+    if (inputValues == null) {
+        return 0.0;
+    }
+    return 1;
+}
